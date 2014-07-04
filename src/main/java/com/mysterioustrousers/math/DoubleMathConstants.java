9770b7a25a124fd16034016785546599668ee7b3
bsara <bsara.dev@gmail.com>
2014-07-04 16:30:57 -0600

    Added some helper classes

---
+++ src/main/java/com/mysterioustrousers/math/DoubleMathConstants.java
@@ -0,0 +1,12 @@
+package com.mysterioustrousers.math;
+
+
+
+public class DoubleMathConstants {
+
+  public static final double TWO_PI    = ((double)Math.PI * (double)2.0);
+  public static final double PI_OVER_2 = ((double)Math.PI / (double)2.0);
+  public static final double PI_OVER_3 = ((double)Math.PI / (double)3.0);
+  public static final double PI_OVER_4 = ((double)Math.PI / (double)4.0);
+  public static final double PI_OVER_6 = ((double)Math.PI / (double)6.0);
+}
---
+++ src/main/java/com/mysterioustrousers/math/FloatMathConstants.java
@@ -0,0 +1,12 @@
+package com.mysterioustrousers.math;
+
+
+
+public class FloatMathConstants {
+
+  public static final float TWO_PI    = ((float)Math.PI * 2.0f);
+  public static final float PI_OVER_2 = ((float)Math.PI / 2.0f);
+  public static final float PI_OVER_3 = ((float)Math.PI / 3.0f);
+  public static final float PI_OVER_4 = ((float)Math.PI / 4.0f);
+  public static final float PI_OVER_6 = ((float)Math.PI / 6.0f);
+}
